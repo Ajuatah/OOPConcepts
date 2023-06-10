81a2e2c8d3eceb67a49bf965442baad5ddf32bba
Andropov Ajuatah Ajebua <ajuatahcodingarena@gmail.com>
2023-06-10 15:06:22 +0100

    Adding encapsulation

---
+++ OOPConceptsC#/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPConceptsC_
+{
+    public   class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name { 
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Your Name is {Name} with {Age}");
+        }
+        
+    }
+}
--- OOPConceptsC#/Program.cs
+++ OOPConceptsC#/Program.cs
@@ -1,15 +1,22 @@
-﻿using System;
+﻿using OOPConceptsC_;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OOPConceptsC_
+namespace OOPConcepts
 {
-    internal class Program
+    public  class Program
     {
         static void Main(string[] args)
         {
+            Person person = new Person();
+            person.Name = "Andropov Ajebua";
+            person.Age = 30;
+            person.PrintInfo();
+
+            Console.ReadKey();
         }
     }
 }
