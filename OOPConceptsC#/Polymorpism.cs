182988431aa3d8510d236411e2da2a7f822fde68
Andropov Ajuatah Ajebua <ajuatahcodingarena@gmail.com>
2023-06-10 16:38:36 +0100

    Added polymorphism

---
+++ OOPConceptsC#/Polymorpism.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Shape1
+{
+    public virtual void Draw1()
+    {
+        Console.WriteLine("Drawing a shape");
+    }
+}
+
+public class Circle1 : Shape1
+{
+    public override void Draw1()
+    {
+        Console.WriteLine("Drawing a circle");
+    }
+}
+
+public class Rectangle1 : Shape1
+{
+    public override void Draw1()
+    {
+        Console.WriteLine("Drawing a rectangle");
+    }
+}
