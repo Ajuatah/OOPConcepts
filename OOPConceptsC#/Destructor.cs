f955820b62220d1c2df04fba2989d6b934f09d2a
Andropov Ajuatah Ajebua <ajuatahcodingarena@gmail.com>
2023-06-10 18:08:13 +0100

    difference between override and new keyword

--- OOPConceptsC#/Destructor.cs
+++ OOPConceptsC#/Destructor.cs
@@ -20,4 +20,29 @@ namespace OOPConceptsC_
             // Perform cleanup operations here
         }
     }
+
+    public class BaseClass1
+    {
+        public void SomeMethod()
+        {
+            // Base class implementation
+        }
+    }
+
+    public class DerivedClass1 : BaseClass1
+    {
+        public new void SomeMethod()
+        {
+            // New implementation in the derived class
+        }
+    }
+
+        /* It's important to understand the difference between 
+        override and new keywords. The override keyword 
+        is used to provide a new implementation for a virtual
+        or abstract member in the derived class, while the new
+        keyword is used to hide a member of the base class with
+        a new implementation in the derived class.
+        */
+
 }
